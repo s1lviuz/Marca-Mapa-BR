044d824c8c00a3031981e0b9a2cdccd594f6a21a
Silvio Junior Carvalho <silviojraraujo@gmail.com>
2022-08-31 18:37:44 -0300

    Adicionando estilo

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ async function startApp() {
         .then((statesData) => statesData.forEach(showStateData))
         mapApiParameters.push(mapKey)
         mapImg.src = mapApiParameters.join('')
+        document.querySelector('section').prepend(mapImg)
     } catch(error) {
         throw(error)
     }
@@ -15,7 +16,7 @@ async function getStatesData() {
         const responseJson = await response.json()
         return responseJson.sort((a, b) => {
             return compareStrings(a.nome, b.nome);
-          })
+        });
     } catch(error) {
         throw(error)
     }
@@ -27,19 +28,24 @@ function compareStrings(a, b) {
     return (a < b) ? -1 : (a > b) ? 1 : 0;
 }
 
+function regionColor (stateData) {
+    const region = stateData.regiao
+    return  (region == 'Norte') ? 'green' : 
+            (region == 'Nordeste') ? 'blue' :
+            (region == 'Sudeste') ? 'yellow' :
+            (region == 'Sul') ? 'orange' : 'red'; 
+}
+
 function showStateData(stateData) {
     let li = document.createElement('li')
     li.innerText = stateData.nome
-    mainElement.append(li)
-    mapApiParameters.push(`markers=color:red%7Csize:small%7C${stateData.latitude},${stateData.longitude}&`)
+    document.querySelector('.states-list').append(li)
+    let color = regionColor(stateData)
+    mapApiParameters.push(`markers=color:${color}%7Csize:small%7C${stateData.latitude},${stateData.longitude}&`)
 }
 
-var mainElement = document.querySelector('main')
-var mapImg = document.querySelector('img')
+var mapImg = document.createElement('img')
 var mapApiParameters = ['https://maps.googleapis.com/maps/api/staticmap?center=Araguaiana,MT&zoom=4&size=500x500&']
 var mapKey = 'key=AIzaSyA4cTSxU1P2UygFzCl0NwCiD7mlZFTVnRg'
 
 startApp();
-
-
-
