6b571f3977c9630117c9b26ecc5b02768a6340a2
Silvio Junior Carvalho <silviojraraujo@gmail.com>
2022-09-03 12:56:33 -0300

    first commit

--- scripts.js
+++ scripts.js
@@ -1,27 +1,61 @@
 async function startApp() {
     try {
-        await getStatesDataFromJson()
-        .then((statesDataArray) => statesDataArray.forEach(insertStateDataOnHTML))
-        mapApiParameters.push(apiKey)
+        await getStatesDataFromApi()
+        .then((statesDataArray) => statesDataArray
+        .forEach((stateData) => {
+            statesList.push({
+                "nome": stateData.nome,
+                "sigla": stateData.sigla,
+                "regiao": stateData.regiao.nome,
+            })
+            getStatesCoordinateByName(stateData.nome)
+            .then((statesCoodinateDada) => {
+            let statesCoodinate = statesCoodinateDada.results
+            console.log(statesCoodinateDada)
+            coordinatesList.push({
+                "latitude": statesCoodinate[0].geometry.location.lat.toFixed(3),
+                "longitude" : statesCoodinate[0].geometry.location.lng.toFixed(3),
+            })
+        })
+        }))
+        insertStateDataOnHTML(statesList,coordinatesList)
+        mapApiParameters.push(googleApiKey)
         mapImg.src = mapApiParameters.join('')
         document.querySelector('section').prepend(mapImg)
     } catch(error) {
         throw(error)
     }
 }
+ 
 
-async function getStatesDataFromJson() {
+async function getStatesDataFromApi() {
     try {
-        const response = await fetch('estados.json')
+        const response = await fetch(ibgeApiURL)
         const responseJson = await response.json()
-        return responseJson.sort((state, nextState) => {
-            let a = state.nome.toLowerCase()
-            let b = nextState.nome.toLowerCase()
-            return (a < b) ? -1 : (a > b) ? 1 : 0;
-        });
+        return responseJson
     } catch(error) {
         throw(error)
     }
+}
+
+async function getStatesCoordinateByName(stateName) {
+    let apiUrlRequest = `${geoApiParameters.replace(' ',stateName.replaceAll(' ','+'))}${googleApiKey}`
+    console.log(apiUrlRequest)
+    try {
+        const response = await fetch(apiUrlRequest)
+        const responseJson = await response.json()
+        return responseJson
+    } catch(error) {
+        throw error
+    }
+}
+
+function timeoutLoading() {
+    setTimeout(() => {
+        if (statesList.length != coordinatesList.length) {
+            timeoutLoading()
+        }
+      }, "1000")
 }
 
 function mapMarkerColor (stateRegion) {
@@ -31,16 +65,34 @@ function mapMarkerColor (stateRegion) {
             (stateRegion == 'Sul') ? 'orange' : 'red'; 
 }
 
-function insertStateDataOnHTML(stateData) {
-    let stateNameElement = document.createElement('li')
-    stateNameElement.innerText = `${stateData.nome} - ${stateData.uf}`
-    document.querySelector('#statesList').append(stateNameElement)
-    let markerColor = mapMarkerColor(stateData.regiao)
-    mapApiParameters.push(`markers=color:${markerColor}%7Csize:small%7C${stateData.latitude},${stateData.longitude}&`)
+function insertStateDataOnHTML(statesNameList, statesCoodinateList) {
+    console.log(statesNameList)
+    statesNameList
+    .forEach((element) => {
+        let stateNameElement = document.createElement('li')
+        stateNameElement.innerText = `${element.nome} - ${element.sigla}`
+        document.querySelector('#statesList').append(stateNameElement)
+    })
+    console.log(statesCoodinateList)
+    for (coordinate of statesCoodinateList) {
+        console.log(statesCoodinateList)
+    }
+    // let stateNameElement = document.createElement('li')
+    // stateNameElement.innerText = `${stateData.nome} - ${stateData.sigla}`
+    // document.querySelector('#statesList').append(stateNameElement)
+    // let stateCoordinate = getStateCoordinateByName(stateData.nome)
+    // let markerColor = mapMarkerColor(stateData.regiao.nome)
+    // mapApiParameters.push(`markers=color:${markerColor}%7Csize:small%7C${stateCoordinate.latitude},${stateCoordinate.longitude}&`)
 }
+
+var statesList = []
+var coordinatesList = []
 
 var mapImg = document.createElement('img')
 var mapApiParameters = ['https://maps.googleapis.com/maps/api/staticmap?center=Araguaiana,MT&zoom=4&size=500x500&']
-var apiKey = 'key=AIzaSyA4cTSxU1P2UygFzCl0NwCiD7mlZFTVnRg'
+var geoApiParameters = 'https://maps.googleapis.com/maps/api/geocode/json?components=locality: &'
+var googleApiKey = 'key=AIzaSyA4cTSxU1P2UygFzCl0NwCiD7mlZFTVnRg'
+
+var ibgeApiURL = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome'
 
 document.addEventListener('DOMContentLoaded', startApp)
